621ae0f45ba5954b526e0e34e6b506092b0e2db0
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-17 14:41:01 +0100

    js layout

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// 1 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età
+
+
+
+// 2 Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+
+// 3 Creare un array di oggetti di studenti
+
+
+// 4 Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+
+// 5 Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
